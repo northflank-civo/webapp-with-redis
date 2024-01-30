c53ceac0dd8210079b1a8af794321104727f6511
Will Stewart <will@northflank.com>
2024-01-30 17:17:44 +0000

    Update server.mjs

--- src/app/server.mjs
+++ src/app/server.mjs
@@ -13,7 +13,7 @@ client.on('error', (err) => {
   }
 });
 
-const TITLE = 'Node.js + Redis on Northflank';
+const TITLE = 'Node.js + Redis on Northflank - Hello World';
 
 const handleIndex = async (ctx, next) => {
   if (connectionErr && initialConnect) {
