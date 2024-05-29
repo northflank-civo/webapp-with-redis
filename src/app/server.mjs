05e58556653466ededcf064ba48bf2c5aa8f47ed
Fred <brix.frederik@gmail.com>
2024-05-29 13:57:34 +0200

    make it easier to demo

--- src/app/server.mjs
+++ src/app/server.mjs
@@ -3,6 +3,7 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 
 import { client, initialConnect } from '../lib/redisConnection.mjs';
+import { getResponseBody } from './body.mjs';
 
 let connectionErr = false;
 let connectionErrMsg;
@@ -12,8 +13,6 @@ client.on('error', (err) => {
     connectionErrMsg = err;
   }
 });
-
-const TITLE = 'Node.js + Redis on Northflank - PR Hello';
 
 const handleIndex = async (ctx, next) => {
   if (connectionErr && initialConnect) {
@@ -25,36 +24,7 @@ const handleIndex = async (ctx, next) => {
   const entries = await client.lrange('films', 0, 100);
   const entryCount = await client.llen('films');
 
-  ctx.body = `
-<html lang="en">
-    <head>
-        <title>${TITLE}</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body class="xl:w-1/2 p-12 mx-auto">
-        <header class="mb-10">
-            <h1 class="text-xl font-semibold mb-5">${TITLE}</h1>
-            <form action="/add" method="POST" class="flex">
-                <input type="text" name="title" class="w-full border-solid border-2 border-black px-3 py-2 mr-1" />
-                <button class="whitespace-nowrap text-white bg-blue-500 border-solid border-2 border-blue-500 px-3 py-2 mr-1">Add entry</button>
-                <a href="/delete" >
-                    <button type="button" class="whitespace-nowrap text-white bg-red-500 border-solid border-2 border-red-500 px-3 py-2">Delete all</button>
-                </a>
-            </form>
-        </header>
-        <div class="italic mb-5">Total number of entries: ${entryCount}</div>
-        <div>
-            <ul>
-                ${entries
-                  .map(
-                    (e) => `<li class="border-t-solid border-t-2 border-black-500 p-5">${e}</li>`
-                  )
-                  .join('')}
-            </ul>
-        </div>
-    </body>
-</html>
-`;
+  ctx.body = getResponseBody(entries, entryCount);
 
   next();
 };
